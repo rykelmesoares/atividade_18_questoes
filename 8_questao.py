f5a0454b46d1a077e3fc09bd6ba1be209b423e43
Kayki-Borges <cicero.oliveira139@aluno.ce.gov.br>
2023-10-16 09:03:21 -0300

    8/18
    atividade

---
+++ 8_questao.py
@@ -0,0 +1,4 @@
+gph = float(input('digite quanto você ganha por hora trabalhada:'))
+ht = float(input('digite quantas horas você trabalha por mês:'))
+salário = gph*ht
+print('ganhando {}R$ por horas trabalhada, após trabalhar {}h por mês, seu salário este mês é de: R$ {}'. format(gph,ht,salário))
