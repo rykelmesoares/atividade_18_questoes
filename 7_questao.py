e8ef9bc7ead613cc71f0a4211d2e75e3ff19e56c
Kayki-Borges <cicero.oliveira139@aluno.ce.gov.br>
2023-10-16 08:30:55 -0300

    7/18
    atividade

---
+++ 7_questao.py
@@ -0,0 +1,5 @@
+base = float(input('digite qual é a medida da base:'))
+altura = float(input('digite qual é a medida da altura:'))
+área = base*altura
+do = área*2
+print('sendo um quadrado de base{}, altura{}, com área{} tem o dobro:{}'. format(base,altura,área,do))
