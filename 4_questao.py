aacf9ba0acf251dc945df747065a81f97db2ea73
Kayki-Borges <cicero.oliveira139@aluno.ce.gov.br>
2023-10-16 08:06:45 -0300

    4/18
    atividade

---
+++ 4_questao.py
@@ -0,0 +1,6 @@
+not1 =float(input('nota do 1 bimestre:'))
+not2 =float(input('nota do 2 bimestre:'))
+not3 =float(input('nota do 3 bimestre:'))
+not4 =float(input('nota do 4 bimestre:'))
+media = (not1+not2+not3+not4)/4
+print('calculando a nota dos bimestres anteriores sua média será: {}'. format(media))
