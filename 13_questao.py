71e5e14f91387da3d96f5a5cd90098a8e8c86425
rykelmesoares <143404132+rykelmesoares@users.noreply.github.com>
2023-10-17 10:48:13 -0300

    Update 13_questao.py
    atividade

--- 13_questao.py
+++ 13_questao.py
@@ -2,4 +2,4 @@ altura = float(input('digite sua altura:'))
 H = (72.7*altura) - 58
 M = (62.1*altura) - 44.7
 
-print('o peso ideal para a sua altura é: {}KG para os homens e {}KG e para mulheres'. format(H,M))
+print('o peso ideal para a altura de homens é: {}KG, e para as mulheres é {}KG'. format(H,M))
