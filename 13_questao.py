9273a24a7005f147583937d4a5f61d138d607f14
M4Noname <cicero.oliveira139@aluno.ce.gov.br>
2023-10-17 11:07:30 -0300

    13/18

--- 13_questao.py
+++
@@ -1,5 +0,0 @@
-altura = float(input('digite sua altura:'))
-H = (72.7*altura) - 58
-M = (62.1*altura) - 44.7
-
-print('o peso ideal para a sua altura é: {}KG para os homens e {}KG e para mulheres'. format(H,M))
