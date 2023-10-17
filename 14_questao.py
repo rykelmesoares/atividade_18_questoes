1d4239bfc3c07b4c477adcc9aa17e44e867df05e
M4Noname <cicero.oliveira139@aluno.ce.gov.br>
2023-10-17 11:01:24 -0300

    14/18
    atividade

---
+++ 14_questao.py
@@ -0,0 +1,4 @@
+peso = float(input('digite o peso dos peixes:'))
+excesso = peso - 50
+multa = excesso*4
+print('foram {}KG em excesso, considerando isso, sua multa será de: {}'. format(excesso,multa))
