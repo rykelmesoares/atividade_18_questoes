3a0301297487b61aaa207b39b307fedc00794110
M4Noname <cicero.oliveira139@aluno.ce.gov.br>
2023-10-17 10:30:11 -0300

    11/18
    atividade

---
+++ 11_questao.py
@@ -0,0 +1,9 @@
+n1 = int(input('digite o primeiro número inteiro:'))
+n2 = int(input('digite o segundo número inteiro:'))
+n3 = float(input('digite um número real:'))
+
+a = (n1*2)*(n2/2)
+b = (n1*3) + n3
+c = n3**3
+
+print(a, b, c)
